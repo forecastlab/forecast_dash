25af121c9ae68ad1a4ec8699ca7d578428ddfa4f
Stephen Tierney <sjtrny@gmail.com>
2019-07-14 15:50:10 +1000

    Refactored layout ahead of dockerising

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:server
+web: gunicorn --chdir ./app app:server
--- create_local_env.sh
+++ create_local_env.sh
@@ -2,4 +2,4 @@ venv_name=venv
 
 virtualenv -p python3 $venv_name
 source $venv_name/bin/activate
-pip install -r requirements.txt
+pip install -r app/requirements.txt

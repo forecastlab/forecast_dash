65aae54086a5329c82738343739cb76b2d5313fa
David Houseman <david@shuttle>
2020-07-23 17:14:01 +1000

    Add naive2 (seasonally-adjusted naive method) from M4 benchmarks.

--- updater/seasadj.R
+++ updater/seasadj.R
@@ -1,17 +1,21 @@
 # This code originates from the M4 Competition Statistical Benchmarks.
 # https://github.com/Mcompetitions/M4-methods/blob/master/Benchmarks%20and%20Evaluation.R
 
+#  f1 <- naive(input, h=fh)$mean #Naive
+#  f2 <- naive_seasonal(input, fh=fh) #Seasonal Naive
+#  f3 <- naive(des_input, h=fh)$mean*SIout #Naive2
+#  f4 <- ses(des_input, h=fh)$mean*SIout #Ses
+#  f5 <- holt(des_input, h=fh, damped=F)$mean*SIout #Holt
+#  f6 <- holt(des_input, h=fh, damped=T)$mean*SIout #Damped
+#  f7 <- Theta.classic(input=des_input, fh=fh)$mean*SIout #Theta
+#  f8 <- (f4+f5+f6)/3 #Comb
+
 library(forecast) #Requires v8.2
 
-naive_seasonal <- function(input, fh){
-  #Used to estimate Seasonal Naive
-  frcy <- frequency(input)
-  frcst <- naive(input, h=fh)$mean 
-  if (frcy>1){ 
-    frcst <- head(rep(as.numeric(tail(input,frcy)), fh), fh) + frcst - frcst
-  }
-  return(frcst)
-}
+# Some example data
+# p <- presidents
+# p[ is.na(p) ] <- 0.0
+# SeasonalityTest( p, 4 )
 
 #Used to determine whether a time series is seasonal
 SeasonalityTest <- function(input, ppy){
@@ -30,27 +34,19 @@ SeasonalityTest <- function(input, ppy){
 }
 
 #Used to estimate the statistical benchmarks of the M4 competition
-Benchmarks <- function(input, fh){
+naive2 <- function(input, fh = 10, level = c(80,95)){
     ppy <- frequency(input)
-    ST <- F
-    if (ppy>1){ ST <- SeasonalityTest(input,ppy) }
-    if (ST==T){
-        Dec <- decompose(input,type="multiplicative")
-        des_input <- input/Dec$seasonal
-        len <- length(Dec$seasonal)
-        SIout <- head(rep(Dec$seasonal[(len-ppy+1):len], fh), fh)
+    ST <- (ppy>1) && SeasonalityTest(input,ppy)
+    if (ST){
+        Dec <- decompose(input,type=type)
+        seasadj <- input/Dec$seasonal
+        SIout <- head(rep(Dec$seasonal[1:ppy], fh), fh)
     }else{
-        des_input <- input
+        seasadj <- input
         SIout <- rep(1, fh)
     }
-    
-    f1 <- naive(input, h=fh)$mean #Naive
-    f2 <- naive_seasonal(input, fh=fh) #Seasonal Naive
-    f3 <- naive(des_input, h=fh)$mean*SIout #Naive2
-    f4 <- ses(des_input, h=fh)$mean*SIout #Ses
-    f5 <- holt(des_input, h=fh, damped=F)$mean*SIout #Holt
-    f6 <- holt(des_input, h=fh, damped=T)$mean*SIout #Damped
-    f8 <- (f4+f5+f6)/3 #Comb
-    
-    return(list(f1,f2,f3,f4,f5,f6,f8))
+    fc <- naive(seasadj, h=fh, level=level)
+    return( list( mean = fc$mean*SIout,
+                  upper = fc$upper*SIout,
+                  lower = fc$lower*SIout) )
 }

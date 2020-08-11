8ef27883992b5c596dd895d604485e0b99b93d70
Leo Fouche <lfou9112@uni.sydney.edu.au>
2020-08-11 17:54:36 +1000

    Changed RNN to output dictionary

--- updater/NNBenchmarks.py
+++ updater/NNBenchmarks.py
@@ -1,17 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 # This code originates from the M4 Competition NN Benchmarks.
 # https://github.com/Mcompetitions/M4-methods/blob/master/ML_benchmarks.py
 
 import numpy as np
+import pandas as pd
 
 from keras.models import Sequential
 from keras.layers import Dense, SimpleRNN
 from keras.optimizers import rmsprop
 
-def create_inputs(y, fh = 10, input_size = 3):
+def create_inputs(y, input_size = 3):
     """
     Creates multiple points to be used as inputs for each step. 
     :param y: an individual time series
-    :param fh: length of forecast
     :param input_size: number of input points for the forecast
     :return:
     """
@@ -28,17 +34,17 @@ def create_inputs(y, fh = 10, input_size = 3):
 
     return x_train, y_train, x_test
 
-def rnn_bench(y, fh = 10, input_size = 3):
+def rnn_bench(y, h = 10, input_size = 3, level = (80,95)):
     """
     Forecasts using 6 SimpleRNN nodes in the hidden layer and a Dense output layer
     :param y: an individual time series
-    :param fh: forecasting horizon
+    :param h: forecasting horizon
     :param input_size: number of points used as input
     :return:
     """
     
     # get inputs 
-    x_train, y_train, x_test = create_inputs(y, fh, input_size)
+    x_train, y_train, x_test = create_inputs(y, input_size)
     
     # reshape to match expected input
     x_train = np.reshape(x_train, (-1, input_size, 1))
@@ -60,12 +66,12 @@ def rnn_bench(y, fh = 10, input_size = 3):
 
     # make predictions
     y_hat = []
-    last_prediction = model.predict(x_test)[0]
-    for i in range(0, fh):
+    last_prediction = float(model.predict(x_test)[0])
+    for i in range(0, h):
         y_hat.append(last_prediction)
         x_test[0] = np.roll(x_test[0], -1)
         x_test[0, (len(x_test[0]) - 1)] = last_prediction
-        last_prediction = model.predict(x_test)[0]
-
-    return np.asarray(y_hat)
+        last_prediction = float(model.predict(x_test)[0])
+    
+    return {'method':'RNN Benchmark', 'mean': y_hat, 'upper': y_hat, 'lower': y_hat}
 

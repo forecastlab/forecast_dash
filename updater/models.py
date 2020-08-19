4d3dee0b304446650d5907f7abd6576429c155ca
Leo Fouche <lfou9112@uni.sydney.edu.au>
2020-08-19 12:14:13 +1000

    Attempt to add RNN to models class

--- updater/models.py
+++ updater/models.py
@@ -7,6 +7,11 @@ from rpy2.robjects.packages import importr
 
 pandas2ri.activate()
 
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, SimpleRNN
+from keras.optimizers import rmsprop
+
 
 class ForecastModel(ABC):
     def __init__(self, h, level):
@@ -35,73 +40,69 @@ class ForecastModel(ABC):
         return {}
 
 
-class NNModel(ForecastModel, ABC):
-    @property
-    @staticmethod
-    @abstractmethod
-    def NN_model_name():
-        pass
-
-    forecast_model_params = {}
+class RNNModel(ForecastModel, ABC):
 
     def __init__(self, h, level):
 
         super().__init__(h, level)
+        self.method = 'SimpleRNN'
 
-        # Import the Python source ??
-        self.forecast_lib = __import__(type(self).forecast_lib)
-        self.forecast_func = getattr(
-                self.forecast_lib, type(self).forecast_model_name
-            )
+    def name():
+        return "RNN M4 Benchmark"
 
     def description(self):
         return self.method
 
-    def get_forecast_dict(self):
-        return dict(
-            self.forecast_lib.forecast(
-                self.fit_results, h=self.h, level=self.level
-            ).items()
-        )
+    def fit(self, y, input_size = 3):
+	
+    	x_train, y_train = y[:-1], np.roll(y, -input_size)[:-input_size]
+    	x_test = np.array( y[-input_size:] ).T[0]
 
-    def fit(self, y):
+    	# reshape input to be [samples, time steps, features] (N-NF samples, 1 time step, 1 feature)
+    	temp_train = np.roll(x_train, -1)
 
-        forecast_dict = self.get_forecast_dict()
+    	for x in range(1, input_size):
+        	x_train = np.concatenate((x_train[:-1], temp_train[:-1]), 1)
+        	temp_train = np.roll(temp_train, -1)[:-1]
+	
+	# reshape to match expected input
+    	x_train = np.reshape(x_train, (-1, input_size, 1))
+    	x_test = np.reshape(x_test, (-1, input_size, 1))
 
-        self.method = forecast_dict["method"]
+    	model = Sequential([
+        	SimpleRNN(6, input_shape=(input_size, 1), activation='linear',
+                  	use_bias=False, kernel_initializer='glorot_uniform',
+                  	recurrent_initializer='orthogonal', bias_initializer='zeros',
+                  	dropout=0.0, recurrent_dropout=0.0),
+        	Dense(1, use_bias=True, activation='linear')
+    	])
+    	opt = rmsprop(lr=0.001)
+    	model.compile(loss='mean_squared_error', optimizer=opt)
 
-    def predict(self):
+    	# fit the model to the training data
 
-        forecast_dict = self.get_forecast_dict()
+    	return model.fit(x_train, y_train, epochs=100, batch_size=1, verbose=0)
 
-        prediction = forecast_dict["mean"]
+    def predict(y, h = self.h, input_size = 3):
+        x_train, y_train = y[:-1], np.roll(y, -input_size)[:-input_size]
+        x_test = np.array( y[-input_size:] ).T[0]
 
-        return prediction
+    	# reshape input to be [samples, time steps, features] (N-NF samples, 1 time step, 1 feature)
+        temp_train = np.roll(x_train, -1)
 
-    def predict_withci(self):
-
-        forecast_dict = self.get_forecast_dict()
-
-        forecast_dict = {"forecast": forecast_dict["mean"]}
-
-        for i in range(len(self.level)):
-            forecast_dict[f"LB_{self.level[i]}"] = forecast_dict["lower"][
-                :, i
-            ]
-            forecast_dict[f"UB_{self.level[i]}"] = forecast_dict["upper"][
-                :, i
-            ]
-
-        return forecast_dict
-
-
-class RNN(NNModel):
-    name = "RNN M4 Benchmark"
-
-    forecast_lib = "NNBenchmarks.py"
-
-    NN_model_name = "rnn_bench"
-
+        for x in range(1, input_size):
+            x_train = np.concatenate((x_train[:-1], temp_train[:-1]), 1)
+            temp_train = np.roll(temp_train, -1)[:-1]
+	
+        y_hat = []
+        last_prediction = float(self.fit().predict(x_test)[0])
+        for i in range(0, h):
+            y_hat.append(last_prediction)
+            x_test[0] = np.roll(x_test[0], -1)
+            x_test[0, (len(x_test[0]) - 1)] = last_prediction
+            last_prediction = float(model.predict(x_test)[0])
+        
+        return y_hat
 
 class RModel(ForecastModel, ABC):
     @property

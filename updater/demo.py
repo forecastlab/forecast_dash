a580eb6371c50771d0439cff95d146bac5db5c82
Stephen Tierney <sjtrny@gmail.com>
2020-08-21 14:04:44 +1000

    Temp save point

---
+++ updater/demo.py
@@ -0,0 +1,128 @@
+import time
+
+from models import RNaive, RNaive2, RAutoARIMA, RSimple, RHolt, RDamped, RTheta
+from rpy2.robjects import pandas2ri
+pandas2ri.activate()
+import json
+import pickle
+from run_models import TimeSeriesRollingSplit, cross_val_score, mean_squared_error
+import pandas as pd
+
+sources_path = "../shared_config/data_sources.json"
+download_dir_path = "../data/downloads"
+
+data_sources_json_file = open(sources_path)
+
+data_sources_list = json.load(data_sources_json_file)
+
+data_source_dict = data_sources_list[0]
+
+print(data_source_dict["title"])
+
+# Read local pickle that we created earlier
+f = open(
+    f"{download_dir_path}/{data_source_dict['title']}.pkl", "rb"
+)
+downloaded_dict = pickle.load(f)
+f.close()
+
+
+
+
+model_class_list = [
+    RNaive,
+    RAutoARIMA,  # RAutoARIMA is very slow!
+    RSimple,
+    RHolt,
+    RDamped,
+    RTheta,
+    RNaive2,
+]
+
+# model_dict = {
+#     model_class.__name__: model_class()
+#     for model_class in model_class_list
+# }
+
+
+
+def run_model_data(model_cls, data_dict, cv, model_params):
+
+    series_df = data_dict["series_df"]
+
+    # Hack to align to the end of the quarter
+    if data_source_dict["frequency"] == "Q":
+        offset = pd.offsets.QuarterEnd()
+        series_df.index = series_df.index + offset
+
+    y = series_df["value"]
+
+    model = model_cls(**model_params)
+
+    cv_score = cross_val_score(model, y, cv, mean_squared_error)
+
+    return cv_score
+
+from multiprocessing import Pool, Manager, cpu_count
+
+
+pool = Pool(cpu_count())
+# pool = Pool(1)
+# print( model_class_list[:1])
+
+forecast_len = 8
+level = [50, 75, 95]
+p_to_use = 1
+cv = TimeSeriesRollingSplit(h=forecast_len, p_to_use=p_to_use)
+model_params = {
+    'h': forecast_len,
+    'level': level
+}
+
+results = pool.starmap(run_model_data, [[model_cls, downloaded_dict, cv, model_params] for model_cls in model_class_list])
+
+print(results)
+
+
+
+# 1. Collect list of model and forecasts
+
+# 2. Run them in parallel
+
+# 3. Process results
+
+
+# for models in model_class_list:
+#     model = model_class(**init_params)
+#
+#
+#
+#
+#
+# model_class = RAutoARIMA
+# s = time.time()
+# model = model_class(**init_params)
+# print(time.time() - s)
+#
+#
+#
+#
+# # def f(x):
+# #     return x*x
+# #
+# # if __name__ == '__main__':
+# #     with Pool(5) as p:
+# #         print(p.starmap(f, [[1], [2], [3]]))
+#
+# def run_model(y, cv):
+#     # return 0
+#     s = time.time()
+#     cv_score = cross_val_score(model, y, cv, mean_squared_error)
+#     print(time.time() - s)
+#     return cv_score
+#
+#
+# # run_model(model, y, cv)
+#
+# pool = Pool(cpu_count())
+# results = pool.starmap(run_model, [[y, cv] for i in range(3)])
--- updater/run_models.py
+++ updater/run_models.py
@@ -11,6 +11,8 @@ from sklearn.utils.validation import indexable, _num_samples
 
 from models import RNaive, RNaive2, RAutoARIMA, RSimple, RHolt, RDamped, RTheta
 
+from multiprocessing import Pool, Manager, cpu_count
+
 pandas2ri.activate()
 
 p_to_use = 1
@@ -19,12 +21,12 @@ level = [50, 75, 95]
 
 model_class_list = [
     RNaive,
-    RAutoARIMA,  # RAutoARIMA is very slow!
+    # RAutoARIMA,  # RAutoARIMA is very slow!
     RSimple,
-    RHolt,
-    RDamped,
-    RTheta,
-    RNaive2,
+    # RHolt,
+    # RDamped,
+    # RTheta,
+    # RNaive2,
 ]
 
 
@@ -105,6 +107,8 @@ class TimeSeriesRollingSplit:
 
 def cross_val_score(model, y, cv, scorer, fit_params={}):
 
+    # print(y)
+
     errors = []
 
     for train_index, test_index in cv.split(y):
@@ -120,89 +124,209 @@ def cross_val_score(model, y, cv, scorer, fit_params={}):
 
 
 def run_models(sources_path, download_dir_path, forecast_dir_path):
-    with open(sources_path) as data_sources_json_file:
 
-        data_sources_list = json.load(data_sources_json_file)
+    return 0
 
-        for data_source_dict in data_sources_list:
+    # Intialise pool
+    # Create a shared data storage pool
+    # Create a grid of combinations between models and series
 
-            print(data_source_dict["title"])
+    pool = Pool(cpu_count())
 
-            # Read local pickle that we created earlier
-            f = open(
-                f"{download_dir_path}/{data_source_dict['title']}.pkl", "rb"
-            )
-            downloaded_dict = pickle.load(f)
-            f.close()
+    # manager = Manager()
 
-            series_df = downloaded_dict["series_df"]
+    # Keys - Series
+    # Values - Dict
+        # Keys - Model Names
+        # Values - Forecast Dict
+            # Model Description - str
+            # CV Score - float
+            # Forecast dataframe
+    # shared_dict = manager.dict()
 
-            # Hack to align to the end of the quarter
-            if data_source_dict["frequency"] == "Q":
-                offset = pd.offsets.QuarterEnd()
-                series_df.index = series_df.index + offset
 
-            cv = TimeSeriesRollingSplit(h=forecast_len, p_to_use=p_to_use)
-            init_params = {"h": forecast_len, "level": level}
-            y = series_df["value"]
-
-            all_forecasts = {}
-
-            # Train a whole bunch-o models on the training set
-            # and evaluate them on the validation set
-            for model_class in model_class_list:
-
-                print("-", model_class.name)
-                forecasted_at = datetime.datetime.now()
-                model = model_class(**init_params)
-                cv_score = cross_val_score(model, y, cv, mean_squared_error)
-
-                model.fit(y)
-                model_name = model.name
-                model_description = model.description()
-
-                # Generate final forecast using best model
-                forecast_dict = model.predict_withci()
-
-                first_value = series_df["value"].iloc[-1]
-                first_time = series_df.index[-1]
-
-                forecast_df = forecast_to_df(
-                    data_source_dict,
-                    forecast_dict,
-                    first_value,
-                    first_time,
-                    forecast_len,
-                    levels=level,
-                )
-
-                all_forecasts[model_name] = {
-                    "model_description": model_description,
-                    "cv_score": cv_score,
-                    "forecast_df": forecast_df,
-                }
-
-            # Store forecast and related
-            data = {
-                "data_source_dict": data_source_dict,
-                "downloaded_dict": downloaded_dict,
-                "forecasted_at": forecasted_at,
-                "all_forecasts": all_forecasts,
-            }
-
-            f = open(
-                f"{forecast_dir_path}/{data_source_dict['title']}.pkl", "wb"
-            )
-            pickle.dump(data, f)
-            f.close()
-
-        # Save statistics
-        print("Generating Statistics")
-        data = {"models_used": [m.name for m in model_class_list]}
-
-        f = open(f"{forecast_dir_path}/statistics.pkl", "wb")
-        pickle.dump(data, f)
-        f.close()
+    # models = [
+    #     model_class()
+    #     for model_class in model_class_list
+    # ]
+    #
+    # with open(sources_path) as data_sources_json_file:
+    #     data_sources_list = json.load(data_sources_json_file)
+    #
+    #
+    # from itertools import product
+    #
+    # series_model_combinations = list(product(data_sources_list, models))
+    #
+    # def run_model(data_source_dict, model):
+    #     # Read local pickle that we created earlier
+    #     f = open(
+    #         f"{download_dir_path}/{data_source_dict['title']}.pkl", "rb"
+    #     )
+    #     downloaded_dict = pickle.load(f)
+    #     f.close()
+    #
+    #     series_df = downloaded_dict["series_df"]
+    #
+    #     # Hack to align to the end of the quarter
+    #     if data_source_dict["frequency"] == "Q":
+    #         offset = pd.offsets.QuarterEnd()
+    #         series_df.index = series_df.index + offset
+    #
+    #     cv = TimeSeriesRollingSplit(h=forecast_len, p_to_use=p_to_use)
+    #     init_params = {"h": forecast_len, "level": level}
+    #     y = series_df["value"]
+    #
+    #     print("-", type(model).name)
+    #
+    #     model = model.set_params(**init_params)
+    #     cv_score = cross_val_score(model, y, cv, mean_squared_error)
+    #
+    #     model.fit(y)
+    #     model_name = model.name
+    #     model_description = model.description()
+    #
+    #     # Generate final forecast using best model
+    #     forecast_dict = model.predict_withci()
+    #
+    #     first_value = series_df["value"].iloc[-1]
+    #     first_time = series_df.index[-1]
+    #
+    #     forecast_df = forecast_to_df(
+    #         data_source_dict,
+    #         forecast_dict,
+    #         first_value,
+    #         first_time,
+    #         forecast_len,
+    #         levels=level,
+    #     )
+    #
+    #     return {
+    #         "model_description": model_description,
+    #         "cv_score": cv_score,
+    #         "forecast_df": forecast_df,
+    #     }
+    #
+    # forecasted_at = datetime.datetime.now()
+    #
+    # #
+    # # with open(sources_path) as data_sources_json_file:
+    # #
+    # #     data_sources_list = json.load(data_sources_json_file)
+    # #
+    # #     for data_source_dict in data_sources_list:
+    # #
+    # #         print(data_source_dict["title"])
+    # #
+    # #         # Read local pickle that we created earlier
+    # #         f = open(
+    # #             f"{download_dir_path}/{data_source_dict['title']}.pkl", "rb"
+    # #         )
+    # #         downloaded_dict = pickle.load(f)
+    # #         f.close()
+    # #
+    # #         series_df = downloaded_dict["series_df"]
+    # #
+    # #         # Hack to align to the end of the quarter
+    # #         if data_source_dict["frequency"] == "Q":
+    # #             offset = pd.offsets.QuarterEnd()
+    # #             series_df.index = series_df.index + offset
+    # #
+    # #         first_value = series_df["value"].iloc[-1]
+    # #         first_time = series_df.index[-1]
+    # #
+    # #         cv = TimeSeriesRollingSplit(h=forecast_len, p_to_use=p_to_use)
+    # #         init_params = {"h": forecast_len, "level": level}
+    # #         y = series_df["value"]
+    # #
+    # #
+    # #         forecasted_at = datetime.datetime.now()
+    # #
+    # #         results = pool.starmap(run_model, [(model_class, init_params, y, cv) for model_class in model_class_list])
+    # #
+    # #         all_forecasts = {}
+    # #
+    # #         for result in results:
+    # #             model_name = result[0]
+    # #             model_description = result[1]
+    # #             forecast_dict = result[2]
+    # #             cv_score = result[3]
+    # #
+    # #             forecast_df = forecast_to_df(
+    # #                 data_source_dict,
+    # #                 forecast_dict,
+    # #                 first_value,
+    # #                 first_time,
+    # #                 forecast_len,
+    # #                 levels=level,
+    # #             )
+    # #
+    # #             all_forecasts[model_name] = {
+    # #                 "model_description": model_description,
+    # #                 "cv_score": cv_score,
+    # #                 "forecast_df": forecast_df,
+    # #             }
+    #
+    #         # all_forecasts = {}
+    #         #
+    #         # # Train a whole bunch-o models on the training set
+    #         # # and evaluate them on the validation set
+    #         # for model_class in model_class_list:
+    #         #
+    #         #     print("-", model_class.name)
+    #         #     forecasted_at = datetime.datetime.now()
+    #         #     model = model_class(**init_params)
+    #         #     cv_score = cross_val_score(model, y, cv, mean_squared_error)
+    #         #
+    #         #     model.fit(y)
+    #         #     model_name = model.name
+    #         #     model_description = model.description()
+    #         #
+    #         #     # Generate final forecast using best model
+    #         #     forecast_dict = model.predict_withci()
+    #         #
+    #         #     first_value = series_df["value"].iloc[-1]
+    #         #     first_time = series_df.index[-1]
+    #         #
+    #         #     forecast_df = forecast_to_df(
+    #         #         data_source_dict,
+    #         #         forecast_dict,
+    #         #         first_value,
+    #         #         first_time,
+    #         #         forecast_len,
+    #         #         levels=level,
+    #         #     )
+    #         #
+    #         #     all_forecasts[model_name] = {
+    #         #         "model_description": model_description,
+    #         #         "cv_score": cv_score,
+    #         #         "forecast_df": forecast_df,
+    #         #     }
+    #
+    #         # Store forecast and related
+    #         data = {
+    #             "data_source_dict": data_source_dict,
+    #             "downloaded_dict": downloaded_dict,
+    #             "forecasted_at": forecasted_at,
+    #             "all_forecasts": all_forecasts,
+    #         }
+    #
+    #         f = open(
+    #             f"{forecast_dir_path}/{data_source_dict['title']}.pkl", "wb"
+    #         )
+    #         pickle.dump(data, f)
+    #         f.close()
+    #
+    #     # Save statistics
+    #     print("Generating Statistics")
+    #     data = {"models_used": [m.name for m in model_class_list]}
+    #
+    #     f = open(f"{forecast_dir_path}/statistics.pkl", "wb")
+    #     pickle.dump(data, f)
+    #     f.close()
+    #
+    # pool.close()
+    # pool.join()
 
 
 if __name__ == "__main__":

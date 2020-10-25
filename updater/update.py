2009d15969ab0d8bdf192a74c6772087100fcf20
Stephen Tierney <sjtrny@gmail.com>
2020-10-25 15:58:40 +1100

    Added cache to docker-compose and cache clearing to updater

--- updater/update.py
+++ updater/update.py
@@ -1,5 +1,7 @@
 from download import download_data
 from run_models import run_models
+import os
+import shutil
 
 print("Downloading Data")
 download_data("../shared_config/data_sources.json", "../data/downloads")
@@ -10,3 +12,11 @@ run_models(
     "../data/downloads",
     "../data/forecasts",
 )
+
+print("Clearing Cache")
+for filename in os.listdir("/nginx_cache"):
+    filepath = os.path.join("/nginx_cache", filename)
+    try:
+        shutil.rmtree(filepath)
+    except OSError:
+        os.remove(filepath)

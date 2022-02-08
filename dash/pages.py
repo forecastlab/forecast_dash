d687d879f1c8b0895b69e644e198035b077d6645
Stephen Tierney <sjtrny@gmail.com>
2022-02-08 15:21:54 +1100

    Homepage series (#120)
    * Add link to search page at bottom and remove link decoration
    
    * Black formatted
    
    * Fix link on homepage

--- dash/pages.py
+++ dash/pages.py
@@ -584,7 +584,7 @@ class Index(BootstrapApp):
                                                         },
                                                     )
                                                 ],
-                                                href=f"/series?{urlencode({'title': feature_series_title})}",
+                                                href=f"/series?{urlencode({'title': "US Unemployment"})}",
                                             ),
                                         ],
                                         lg=8,

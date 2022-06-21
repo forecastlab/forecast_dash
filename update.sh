741ce35913c731452aeeba926459f01d0b56c583
Ardalan Mirzaei <ardalan.mirzaei@sydney.edu.au>
2022-06-21 13:22:21 +1000

    intial push
    This is for issue #41 to minimise downtime.

--- update.sh
+++ update.sh
@@ -3,5 +3,10 @@
 git reset --hard HEAD
 git pull
 touch shared_config/acme.json & chmod 600 shared_config/acme.json
+docker-compose down -f docker-compose.traefik.yml 
 docker-compose down
-docker-compose up --force-recreate --build -d
+docker-compose up -f docker-compose.traefik.yml --force-recreate --build -d
+# docker-compose up --force-recreate --build -d
+chmod 600 ./deployment.sh
+. ./deployment.sh # run in global?
+webhook -hooks hooks.json & # ensure installed: sudo apt-get install webhook
